5e4e9782bd085b1a36e6af8be51fcec09f48b5bf
Jurij Kos <jurij.kos1996@live.com>
2019-01-08 11:52:19 +0100

    Updates

---
+++ precache-manifest.970b09cf5142efa364a51366d70b984b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b46b108bc7916c25dc4a",
+    "url": "/mobius/static/js/runtime~main.b46b108b.js"
+  },
+  {
+    "revision": "e4f1488f93952d5e0fde",
+    "url": "/mobius/static/js/main.e4f1488f.chunk.js"
+  },
+  {
+    "revision": "7b258e9e795569399f99",
+    "url": "/mobius/static/js/1.7b258e9e.chunk.js"
+  },
+  {
+    "revision": "e4f1488f93952d5e0fde",
+    "url": "/mobius/static/css/main.2953ab02.chunk.css"
+  },
+  {
+    "revision": "7b258e9e795569399f99",
+    "url": "/mobius/static/css/1.6147fa94.chunk.css"
+  },
+  {
+    "revision": "25774b06d6f925b8c4d0c8bc47f8f89c",
+    "url": "/mobius/index.html"
+  }
+];
